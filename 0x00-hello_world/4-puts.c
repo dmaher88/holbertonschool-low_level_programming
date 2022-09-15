e0d59075bf94ad18b834fe739f0bd35cecfb15e7
dmaher88 <dmaher88@github.com>
2022-09-15 17:23:54 -0500

    Part 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* displaying the string in the stdout */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
