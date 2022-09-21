e5769f4f3a97184495ec2319336afa3e6933e66f
dmaher88 <dmaher88@github.com>
2022-09-20 23:53:21 -0500

    Part 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet followed by a new line
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char alphab[26] = "abcdefghijklmnopqrstuvzxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphab[i]);
+	}
+	putchar('\n');
+	return (0);
+}
